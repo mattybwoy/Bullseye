66a52d6361bb59dff8799a9f2e699e8c0289fff3
Matthew Lock <matthew_lock20@hotmail.com>
2021-05-13 18:31:57 +0100

    Added text, slider and button to game

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50.00), in: 1.0...100.00)
+                Text("100")
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
+
