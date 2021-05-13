82f9f931b201344078f8a97d510d049b2dd38efd
Matthew Lock <matthew_lock20@hotmail.com>
2021-05-13 17:18:56 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Matthew Lock on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
