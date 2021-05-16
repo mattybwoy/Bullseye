71a2a059eedfccb29e49ea66d3ea921fd4866e4a
Matthew Lock <matthew_lock20@hotmail.com>
2021-05-16 10:06:05 +0100

    Slider now connected to state

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,13 +11,15 @@ struct ContentView: View {
     
     @State private var alertIsVisible: Bool = false
     
+    @State private var sliderValue: Double = 50.0
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE YOU CAN TO").bold().kerning(2).multilineTextAlignment(.center).lineSpacing(4.0).font(.footnote)
             Text("89").kerning(-1).font(.largeTitle).fontWeight(.black)
             HStack {
                 Text("1").bold()
-                Slider(value: .constant(50.00), in: 1.0...100.00)
+                Slider(value: self.$sliderValue, in: 1.0...100.00)
                 Text("100").bold()
             }
             Button(action: {self.alertIsVisible = true}) {
