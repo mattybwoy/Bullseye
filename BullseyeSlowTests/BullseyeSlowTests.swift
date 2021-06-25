35eea540e9c16344612a4fa81119a9d8b5628655
Matthew Lock <matthew_lock20@hotmail.com>
2021-06-25 11:54:47 +0100

    Added Asynchronous code test, pass

---
+++ BullseyeSlowTests/BullseyeSlowTests.swift
@@ -0,0 +1,57 @@
+//
+//  BullseyeSlowTests.swift
+//  BullseyeSlowTests
+//
+//  Created by Matthew Lock on 25/06/2021.
+//
+
+import XCTest
+@testable import Bullseye
+
+var sut: URLSession!
+
+class BullseyeSlowTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = URLSession(configuration: .default)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+    // Asynchronous test: success fast, failure slow
+    func testValidApiCallGetsHTTPStatusCode200() throws {
+      // given
+      let urlString =
+        "http://www.randomnumberapi.com/api/v1.0/random?min=0&max=100&count=1"
+      let url = URL(string: urlString)!
+      // 1
+      let promise = expectation(description: "Status code: 200")
+
+      // when
+      let dataTask = sut.dataTask(with: url) { _, response, error in
+        // then
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+          if statusCode == 200 {
+            // 2
+            promise.fulfill()
+          } else {
+            XCTFail("Status code: \(statusCode)")
+          }
+        }
+      }
+      dataTask.resume()
+      // 3
+      wait(for: [promise], timeout: 5)
+    }
+
+
+}
